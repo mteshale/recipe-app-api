32e51d59d71e901d78547391ce5cf96e1bd4adb3
Mengist <mengist.teshale@gmail.com>
2026-04-12 17:01:12 -0600

    Adding Database Migration

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,10 @@
 Calculator functions
 """
 
-
-def add(x,y):
+def add(x, y):
     """Add x and y and ruturn results"""
     return x + y
 
-def substruct(x,y):
+def substruct(x, y):
     """Substruct x from y and ruturn results"""
     return y - x
