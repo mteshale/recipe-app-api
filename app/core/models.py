3377d58aab09b816dfc40638886e02cd16241e5b
Mengist <mengist.teshale@gmail.com>
2026-05-01 23:35:26 -0600

    Final

--- app/core/models.py
+++ app/core/models.py
@@ -11,12 +11,14 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
+
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image."""
     ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'recipe', filename)
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -70,8 +72,6 @@ class Recipe(models.Model):
     ingredients = models.ManyToManyField("Ingredient")
     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
-
-
     def __str__(self):
         return self.title
 
@@ -100,4 +100,3 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
-
